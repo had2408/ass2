869edf7d03f41a1189ce97bc3439c39077e9b859
had2408 <anhduy2408@gmail.com>
2017-06-16 17:03:17 +0700

    project

---
+++ assignment/assignment/defs.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <string.h>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#define NUM_ITERATIONS "trn_params.num_iterations:"
+#define NUM_FOLDS "trn_params.num_folds:"
+#define NUM_LEARNING_RATE "trn_params.learning_rate:"
+#define NUM_START_A "trn_params.start_a:"
+#define NUM_START_B "trn_params.start_b:"
+
+using namespace std;
---
+++ assignment/assignment/main.cpp
@@ -0,0 +1,15 @@
+#ifndef _definition_h_
+#include "defs.h"
+#define _definition_h_
+#endif
+
+#define SIZE 100000
+#define WITDH 10
+#define INPUTFILE "data.2.txt"
+#define OUTFILE "std.2.txt"
+
+int main(void) {
+	cout << "done \n";
+	system("PAUSE");
+	return 0;
+}
